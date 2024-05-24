56623a827ad07eb55149772daa7706abef615e1c
Brandon <wongbr55@LAPTOP-4JK7BIBR>
2024-05-24 10:40:28 -0400

    Took away need to activate conda envrionment

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# activate conda envrionment
-conda activate rde2
 # segment figures
 python figure_segmentation/image_segmentation.py --imgpath $1 --reactfilename reactaction --substratefilename substrates
 # call reactiondataextractor2 on both reaction and substrate images
@@ -9,5 +7,3 @@ python reactiondataextractor2/reactiondataextractor/extract.py --path ./reaction
 python reactiondataextractor2/reactiondataextractor/extract.py --path ./products.jpeg --outdir ./
 # combine both jsons into one final json
 python figure_segmentation/json_parsing.py --reactantspath ./reaction.json --substratepath ./substrates.json
-# deactivate json
-conda deactivate
