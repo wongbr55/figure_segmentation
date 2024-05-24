da1d5901d22008ece8a1871670cad4dbcb881faa
Brandon <wongbr55@LAPTOP-4JK7BIBR>
2024-05-24 15:45:58 -0400

    Passed standard error to dev/null in run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ echo "Segmenting figures..."
 python figure_segmentation/image_segmentation.py --imgpath $1 --reactfilename reaction --substratefilename substrates
 # call reactiondataextractor2 on both reaction and substrate images
 echo "Extracting SMILEs from reaction..."
-python reactiondataextractor2/reactiondataextractor/extract.py --path ./reaction.jpeg --output_dir ./ > /dev/null
+python reactiondataextractor2/reactiondataextractor/extract.py --path ./reaction.jpeg --output_dir ./ > /dev/null 2> /dev/null
 echo "Extracting SMILEs from substrates..."
-python reactiondataextractor2/reactiondataextractor/extract.py --path ./substrates.jpeg --output_dir ./ > /dev/null
+python reactiondataextractor2/reactiondataextractor/extract.py --path ./substrates.jpeg --output_dir ./ > /dev/null 2> /dev/null
 # combine both jsons into one final json
 echo "Combining jsons..."
 python figure_segmentation/json_parsing.py --reactantspath ./reaction.json --substratepath ./substrates.json
