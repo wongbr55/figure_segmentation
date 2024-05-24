c2f1eadbad2f00c598e4168d4ca53183f0da4739
Brandon <wongbr55@LAPTOP-4JK7BIBR>
2024-05-24 11:06:22 -0400

    Fixed argument issue in run.sh for reactiondataextractor

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # segment figures
 python figure_segmentation/image_segmentation.py --imgpath $1 --reactfilename reactaction --substratefilename substrates
 # call reactiondataextractor2 on both reaction and substrate images
-python reactiondataextractor2/reactiondataextractor/extract.py --path ./reaction.jpeg --outdir ./
-python reactiondataextractor2/reactiondataextractor/extract.py --path ./products.jpeg --outdir ./
+python reactiondataextractor2/reactiondataextractor/extract.py --path ./reaction.jpeg --output_dir ./
+python reactiondataextractor2/reactiondataextractor/extract.py --path ./products.jpeg --output_dir ./
 # combine both jsons into one final json
 python figure_segmentation/json_parsing.py --reactantspath ./reaction.json --substratepath ./substrates.json
